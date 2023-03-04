d1c7df5aa8d97b68356a75b563ac3a830fd61f56
shankar-trainer <shankar7979@hotmail.com>
2023-03-04 14:30:06 +0530

    3rd commit

--- src/test/java/org/com/capegemini/model/StudentTest.java
+++ src/test/java/org/com/capegemini/model/StudentTest.java
@@ -8,11 +8,13 @@ import org.junit.gen5.api.Test;
 class StudentTest {
    Student stud1;
    Student stud2;
+   Student stud3;
 
    @BeforeEach
 	public void setUp() throws Exception {
 	   stud1=new Student(1001, "mohan", "math", 80);
 	   stud2=new Student(1001, "mohan", "math", 80);
+	   stud3=new Student(1002, "sohan", "math", 80);
 	}
 
 	@AfterEach
@@ -23,6 +25,7 @@ class StudentTest {
 	public	void test() {
 		 Assertions.assertEquals(stud1.getRoll(), 1001);
 		 Assertions.assertEquals(stud1, stud2);
+		 Assertions.assertEquals(stud1, stud3);
 	}
 
 }
